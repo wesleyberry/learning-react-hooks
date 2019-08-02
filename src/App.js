929fa73a594fc36161f3de3c324489af7fede597
Wesley Berry <wesleyberry52@gmail.com>
2019-08-01 19:18:54 -0500

    Can now 'complete' todos in todo app

--- src/App.js
+++ src/App.js
@@ -58,13 +58,15 @@ function App() {
 
   const completeTodo = index => {
     const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
   }
 
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo}/>
         ))}
         <TodoForm addTodo={addTodo}/>
       </div>
